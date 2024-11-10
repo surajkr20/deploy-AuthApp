895923d981eab5c975aca94de781feffa215f263
Suraj Kumar <155707604+surajkr20@users.noreply.github.com>
2024-11-10 17:03:25 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
       return handleError("Email and Password are required");
     }
     try {
-      const url = "http://localhost:5000/auth/login";
+      const url = "deploy-auth-app-backend.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST", // use uppercase
         headers: {
